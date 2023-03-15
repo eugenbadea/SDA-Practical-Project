63eaa550dfcf4d1f0f93606a55cb2ca82aa2c13b
EUGEN2037 <badea4@gmail.com>
2023-03-15 20:12:04 +0200

    Repository class

---
+++ SDA-Practical-Project/src/main/java/com/sda/practicalproject/repository/exception/EntityUpdateFailedException.java
@@ -0,0 +1,8 @@
+package com.sda.practicalproject.repository.exception;
+
+public class EntityUpdateFailedException extends Exception{
+
+    public EntityUpdateFailedException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
