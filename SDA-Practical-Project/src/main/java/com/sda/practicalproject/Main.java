896ba152593c79091848cd1f53014f3d6d4a1ed7
EUGEN2037 <badea4@gmail.com>
2023-03-18 20:51:38 +0200

    PCM-4 implement menu

--- SDA-Practical-Project/src/main/java/com/sda/practicalproject/Main.java
+++ SDA-Practical-Project/src/main/java/com/sda/practicalproject/Main.java
@@ -1,5 +1,6 @@
 package com.sda.practicalproject;
 
+import com.sda.practicalproject.controler.menu.MenuItem;
 import com.sda.practicalproject.model.Vet;
 import com.sda.practicalproject.repository.VetRepository;
 import com.sda.practicalproject.repository.VetRepositoryImpl;
@@ -7,12 +8,58 @@ import com.sda.practicalproject.repository.exception.EntityUpdateFailedException
 import com.sda.practicalproject.utils.SessionManager;
 import jdk.swing.interop.SwingInterOpUtils;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) throws EntityUpdateFailedException {
-     
+        Scanner scanner = new Scanner(System.in);
+        SessionManager.getSessionFactory();
+
+        for (int i = 1; i <= 100; i++) {
+            System.out.println("///////////////////////////////////////////////");
+        }
 
 
+        MenuItem selectedOption = MenuItem.UNKNOWN;
 
+        while (selectedOption != MenuItem.EXIT) {
+            System.out.println("");
+            MenuItem.printMenuItems();
+            System.out.println("Please select an option:");
+            try {
+                int numericOption = Integer.parseInt(scanner.nextLine());
+                selectedOption = MenuItem.searchByOption(numericOption);
+            } catch (NumberFormatException e) {
+                System.out.println("Please use numeric value");
+                selectedOption=MenuItem.UNKNOWN;
+            }
+
+
+            switch (selectedOption) {
+                case ADD_VET:
+                    System.out.println("Add vet not implemented");
+                    break;
+                case UPDATE_VET:
+                    System.out.println("Update vet not implemented");
+                    break;
+                case DELETE_VET:
+                    System.out.println("Delete vet not implemented");
+                    break;
+                case VIEW_VET_LIST:
+                    System.out.println("View vet list nor implemented");
+                    break;
+                case EXIT:
+                    System.out.println("Good bye!");
+                    break;
+                case UNKNOWN:
+                    System.out.println("Please insert a valid option");
+                    break;
+                default:
+                    System.out.println("Option not implemented");
+                    break;
+            }
+        }
+        System.out.println();
         SessionManager.shutdown();
     }
 }
